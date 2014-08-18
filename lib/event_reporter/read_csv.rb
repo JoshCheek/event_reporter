af1d5c56527576117f35f15f330b3cb09428ae70
Josh Cheek <josh.cheek@gmail.com>
2014-08-18 03:25:03 -0600

    Implement tests around the filename by moving behaviour into an instance

--- lib/event_reporter/read_csv.rb
+++ lib/event_reporter/read_csv.rb
@@ -2,7 +2,17 @@ require 'csv'
 
 module EventReporter
   class ReadCsv
-    def self.call(filename)
+    def self.call(filename=nil)
+      new(filename).call
+    end
+
+    attr_accessor :filename
+
+    def initialize(filename=nil)
+      self.filename = (filename || 'event_attendees.csv')
+    end
+
+    def call
       filepath = File.expand_path("../../../data/#{filename}", __FILE__)
       CSV.readlines(filepath, headers: true, header_converters: :downcase) # glanced through docs to get this http://rdoc.info/stdlib/csv/CSV#HeaderConverters-constant
     end
