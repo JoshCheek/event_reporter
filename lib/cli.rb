545cfc6758b4280de862f122197839fb3a7cc24f
Josh Cheek <josh.cheek@gmail.com>
2014-08-18 01:49:11 -0600

    Slightly further along the first test

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,9 +1,28 @@
+require 'csv'
+class ReadCsv
+  def self.call(filename)
+    filepath = File.expand_path("../../data/#{filename}", __FILE__)
+    CSV.readlines(filepath)
+  end
+end
+
 class CLI
+  attr_accessor :queue, :pristine_data
+
+  def initialize
+    self.queue         = []
+    self.pristine_data = []
+  end
+
   def process(line)
     command, *arguments = line.split
     case command
     when 'load'
-      self.queue = ReadCsv.call arguments.first
+      self.pristine_data = ReadCsv.call arguments.first
+    when 'queue'
+      if arguments.first == 'count'
+        queue.count
+      end
     end
   end
 end
